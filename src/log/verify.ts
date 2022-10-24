26925f5a17ce7592cd19905535635027ba600e88
ArvinWei <clovery.org@gmail.com>
2022-10-24 08:37:57 +0800

    🦄 refactor: 增加返回格式定义

--- src/log/verify.ts
+++ src/log/verify.ts
@@ -6,9 +6,10 @@ import { fetchAccessToken } from '../token'
 type VerifyInput = {
   pageNo: number
   pageSize: number
+  userInfo: string
 }
 
-export async function verify(input: VerifyInput): Promise<any> {
+export async function verify(input: VerifyInput): Promise<VerifyResponse> {
   const access_token = await fetchAccessToken()
   const url = `${baseUrl}/log/verify/pageable`
 
@@ -17,9 +18,64 @@ export async function verify(input: VerifyInput): Promise<any> {
     method: 'GET',
     params: {
       access_token,
+      ...input,
       ...payload
     }
   })
 
   return response.data
+}
+
+export interface VerifyResponse {
+  success: boolean
+  page: Page
+  error_code: number
+  error_msg: string
+}
+
+export interface Page {
+  pageNo: number
+  pageSize: number
+  totalCount: number
+  hasMore: boolean
+  result: Result[]
+}
+
+export interface Result {
+  logId: string
+  userId: string
+  internalNum: any
+  name?: string
+  cardId: any
+  identityType: string
+  avatarKey?: string
+  avatarPath?: string
+  groupName?: string
+  snapshotKey: string
+  snapshotPath: string
+  checkTimeDate: string
+  checkType: string
+  passStatus: string
+  noPassReason?: string
+  deviceId: string
+  deviceName: string
+  deviceType: string
+  deviceCategory: string
+  temperature: any
+  temperatureStatus: any
+  maskStatus: any
+  healthStatus: any
+  routeInfo: any
+  customPersonId: any
+  customExtends: any
+  similarityScore: number
+  channelId: any
+  channelName: any
+  messageId: string
+  age: number
+  gender: string
+  deviceLocation: string
+  natInfo: string
+  antInfo: string
+  vacInfo: string
 }
